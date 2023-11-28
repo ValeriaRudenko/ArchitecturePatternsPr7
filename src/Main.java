2fbc04da2f6308a0fa0a1c21faae73a3b2e88815
Francisco Javier Garcia Gonzalez <alu0101030562@ull.edu.es>
2023-11-28 18:12:32 +0000

    cambios en el main

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,35 @@
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import es.ull.patrones.api.VintedApiSubject;
+import es.ull.patrones.api.ParseJSON;
+import es.ull.patrones.observer.Observer;
+import es.ull.patrones.observer.PrintObserver;
+
+import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) throws IOException, InterruptedException {
-   HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create("https://vinted3.p.rapidapi.com/getSearch?country=es&page=1&order=relevance&keyword=the%20last%20of%20us%202&minPrice=15&maxPrice=50"))
-            .header("X-RapidAPI-Key", "fea5410740msha9d87817c8480e4p14a43ajsn72b7e0f19839")
-            .header("X-RapidAPI-Host", "vinted3.p.rapidapi.com")
-            .method("GET", HttpRequest.BodyPublishers.noBody())
-            .build();
-    HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Ingrese la página: ");
+    int pagina = scanner.nextInt();
+    System.out.print("Ingrese el precio mínimo: ");
+    int precioMin = scanner.nextInt();
 
-    // Patrón para encontrar comas fuera de corchetes
-    Pattern pattern = Pattern.compile("(?<=\\})(,)(?=\\{|$)");
-    //Pattern pattern = Pattern.compile("(?<=\\})(,)(?=\"){0,1}");
+    System.out.print("Ingrese el precio máximo: ");
+    int precioMax = scanner.nextInt();
+    scanner.nextLine();
+    System.out.print("Ingrese el objeto a buscar: ");
+    String objeto = scanner.nextLine();
 
-    // Utiliza un Matcher para encontrar coincidencias
-    Matcher matcher = pattern.matcher(response.body());
+    // Crear instancia del sujeto
+    VintedApiSubject vintedApiSubject = new VintedApiSubject();
 
-    // Usa el método appendReplacement para reemplazar las comas fuera de corchetes
-    StringBuffer result = new StringBuffer();
-    while (matcher.find()) {
-      matcher.appendReplacement(result, "\n");
-    }
-    matcher.appendTail(result);
+    // Crear instancia del observador
+    Observer printObserver = new PrintObserver();
 
-    // Imprime el resultado
-    System.out.println(result);
-
+    // Agregar el observador al sujeto
+    vintedApiSubject.addObserver(printObserver);
+    System.out.println("pagina: " + pagina + "\nprecio min: " + precioMin + "\nprecio max: " + precioMax + "\nArticulo: " + objeto);
+    // Realizar la solicitud HTTP y notificar a los observadores
+    vintedApiSubject.fetchData(pagina, objeto, precioMin, precioMax);
   }
 }
 
