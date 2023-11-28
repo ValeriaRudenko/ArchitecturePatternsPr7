3089e351c793a41ec8775e38eb46d84281ea5783
rrrguez <alu0101394763@ull.edu.es>
2023-11-28 14:56:12 +0000

    Added search button and changed main frame layout

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -3,11 +3,19 @@ import java.awt.*;
 
 import java.awt.Image;
 import javax.swing.ImageIcon;
+import javax.swing.border.Border;
 
 public class MainFrame extends JFrame {
     private JPanel filtersPanel; // Panel to place the filter fields
     private JPanel buttonsPanel; // Panel to place the buttons
     private JPanel logoPanel;
+
+    private JLabel noOfFavouritesLabel;
+    private JLabel keywordLabel;
+    private JLabel countryLabel;
+    private JLabel noOfpageLabel;
+    private JLabel minPriceLabel;
+    private JLabel maxPriceLabel;
 
     private JTextField noOfFavourites;
     private JTextField keyword;
@@ -18,13 +26,16 @@ public class MainFrame extends JFrame {
 
     private JLabel vintedLogo;
 
+    private JButton searchButton;
+
     // Constructor method
     public MainFrame() {
         this.setSize(500, 500);
-        this.getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+        this.getContentPane().setLayout(new BorderLayout());
+        // this.getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
         this.setBackground(new Color(190, 245, 255));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("Vinted");
+        this.setTitle("DAP - Practise 7 - Observer Pattern");
 
         // Icon of the main frame
         ImageIcon vintedIcon = new javax.swing.ImageIcon("images/vinted_icon.png");
@@ -34,38 +45,60 @@ public class MainFrame extends JFrame {
         // Configure panel stuff
         filtersPanel = new JPanel();
         filtersPanel.setBackground(new Color(190, 245, 255));
-        // filtersPanel.setLayout(new BorderLayout());
         buttonsPanel = new JPanel();
-        filtersPanel.setBackground(new Color(190, 245, 255));
+        buttonsPanel.setBackground(new Color(190, 245, 255));
         logoPanel = new JPanel();
-
-        // Text fields
-        keyword = new JTextField("Keyword");
-        noOfpage = new JTextField("Page");
-        country = new JTextField("Country");
-        minPrice = new JTextField("Mininum price");
-        maxPrice = new JTextField("Maximum price");
-        noOfFavourites = new JTextField("Minimum number of favourites");
+        logoPanel.setSize(150, 100);
+        logoPanel.setLayout(new BorderLayout());
+        logoPanel.setBackground(new Color(190, 245, 255));
 
         // Add logo to the label that will be on the top of the window
-        ImageIcon logo = new ImageIcon("images/Vinted_Logo.png"); // Reemplaza con la ruta de tu imagen
+        ImageIcon logo = new ImageIcon("images/Vinted_Logo.png");
         Image originalIcon = logo.getImage();
-        Image escalatedLogo = originalIcon.getScaledInstance(200, 65, Image.SCALE_SMOOTH); // Ajusta el tamaño
-        ImageIcon escalatedLogoDef = new ImageIcon(escalatedLogo);
-        vintedLogo = new JLabel(escalatedLogoDef);
-        logoPanel.add(vintedLogo);
+        Image escalatedLogo = originalIcon.getScaledInstance(150, 50, Image.SCALE_SMOOTH);
+        logo = new ImageIcon(escalatedLogo);
+        vintedLogo = new JLabel(logo);
+        logoPanel.add(vintedLogo, BorderLayout.CENTER);
+
+        // Search labels
+        keywordLabel = new JLabel("Keyword: ");
+        noOfpageLabel = new JLabel("No. of page: ");
+        countryLabel = new JLabel("Country: ");
+        minPriceLabel = new JLabel("Min. price: ");
+        maxPriceLabel = new JLabel("Max. price: ");
+        noOfFavouritesLabel = new JLabel("No. of favourites: ");
+
+        // Search text fields
+        keyword = new JTextField(20);
+        noOfpage = new JTextField(20);
+        country = new JTextField(20);
+        minPrice = new JTextField(10);
+        maxPrice = new JTextField(10);
+        noOfFavourites = new JTextField(20);
+
+        // Search button
+        // When pressed, the program will use the Vinted API
+        searchButton = new JButton("Search items");
 
         // Add stuff to the panels and main frame
+        filtersPanel.add(keywordLabel);
         filtersPanel.add(keyword);
+        filtersPanel.add(noOfpageLabel);
         filtersPanel.add(noOfpage);
+        filtersPanel.add(countryLabel);
         filtersPanel.add(country);
+        filtersPanel.add(minPriceLabel);
         filtersPanel.add(minPrice);
+        filtersPanel.add(maxPriceLabel);
         filtersPanel.add(maxPrice);
+        filtersPanel.add(noOfFavouritesLabel);
         filtersPanel.add(noOfFavourites);
 
-        this.add(logoPanel);
-        this.add(filtersPanel);
-        this.add(buttonsPanel);
+        buttonsPanel.add(searchButton);
+
+        this.add(logoPanel, BorderLayout.NORTH);
+        this.add(filtersPanel, BorderLayout.CENTER);
+        this.add(buttonsPanel, BorderLayout.SOUTH);
 
         this.setVisible(true);
         filtersPanel.setVisible(true);
