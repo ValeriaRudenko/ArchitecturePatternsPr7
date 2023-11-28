976ac5689f4175b8eefa2d762b6497896808d7b4
rrrguez <alu0101394763@ull.edu.es>
2023-11-28 18:32:44 +0000

    Added combo boxes to the filters panel

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -2,37 +2,43 @@ import javax.swing.*;
 import java.awt.*;
 
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.border.Border;
 
 public class MainFrame extends JFrame {
+    // Panels
     private JPanel filtersPanel; // Panel to place the filter fields
     private JPanel buttonsPanel; // Panel to place the buttons
     private JPanel logoPanel;
 
+    // Labels
     private JLabel noOfFavouritesLabel;
     private JLabel keywordLabel;
     private JLabel countryLabel;
     private JLabel noOfpageLabel;
     private JLabel minPriceLabel;
     private JLabel maxPriceLabel;
-
-    private JTextField noOfFavourites;
-    private JTextField keyword;
-    private JTextField country;
-    private JTextField noOfpage;
-    private JTextField minPrice;
-    private JTextField maxPrice;
-
+    private JLabel message;
     private JLabel vintedLogo;
 
+    // Input fields
+    private JTextField keyword;
+    private JComboBox<String> country;
+    private JComboBox<Integer> noOfpage;
+    private JComboBox<String> minPrice;
+    private JComboBox<String> maxPrice;
+    private JComboBox<Integer> noOfFavourites;
+
+    // Buttons
     private JButton searchButton;
 
     // Constructor method
     public MainFrame() {
-        this.setSize(500, 500);
+        // Configuration of the main frame
+        this.setSize(400, 300);
         this.getContentPane().setLayout(new BorderLayout());
-        // this.getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
         this.setBackground(new Color(190, 245, 255));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("DAP - Practise 7 - Observer Pattern");
@@ -45,6 +51,7 @@ public class MainFrame extends JFrame {
         // Configure panel stuff
         filtersPanel = new JPanel();
         filtersPanel.setBackground(new Color(190, 245, 255));
+        filtersPanel.setLayout(new GridLayout(7, 2));
         buttonsPanel = new JPanel();
         buttonsPanel.setBackground(new Color(190, 245, 255));
         logoPanel = new JPanel();
@@ -55,32 +62,48 @@ public class MainFrame extends JFrame {
         // Add logo to the label that will be on the top of the window
         ImageIcon logo = new ImageIcon("images/Vinted_Logo.png");
         Image originalIcon = logo.getImage();
-        Image escalatedLogo = originalIcon.getScaledInstance(150, 50, Image.SCALE_SMOOTH);
+        Image escalatedLogo = originalIcon.getScaledInstance(75, 25, Image.SCALE_SMOOTH);
         logo = new ImageIcon(escalatedLogo);
         vintedLogo = new JLabel(logo);
         logoPanel.add(vintedLogo, BorderLayout.CENTER);
 
         // Search labels
-        keywordLabel = new JLabel("Keyword: ");
-        noOfpageLabel = new JLabel("No. of page: ");
-        countryLabel = new JLabel("Country: ");
-        minPriceLabel = new JLabel("Min. price: ");
-        maxPriceLabel = new JLabel("Max. price: ");
-        noOfFavouritesLabel = new JLabel("No. of favourites: ");
+        message = new JLabel("Please enter the search criteria:");
+        keywordLabel = new JLabel("Keyword");
+        noOfpageLabel = new JLabel("No. of page");
+        countryLabel = new JLabel("Country");
+        minPriceLabel = new JLabel("Min. price (€)");
+        maxPriceLabel = new JLabel("Max. price (€)");
+        noOfFavouritesLabel = new JLabel("Min. no. of favourites");
+
+        // Combo boxes values
+        Integer[] pages = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+                26, 27, 28, 29, 30, 31, 32}; // You can only select 1-32 page
+        String[] countryOptions = {"France", "Belgium", "Spain", "Luxembourg", "Netherlands", "Lithuania", "Germany",
+                "Austria", "Italy", "United Kingdom", "Portugal", "United States", "Czech Republic", "Slovakia",
+                "Poland", "Sweden", "Romania", "Hungary", "Denmark"};
+        String[] prices = {"None", "0", "1", "2", "3", "4", "5", "6", "8", "10", "15", "20", "25", "30", "35", "40",
+                "45", "50", "60", "70", "80", "90", "100", "150", "200", "250", "300", "400", "500", "750", "1000",
+                "1500", "2000", "2500", "3000", "4000", "5000", "7500", "10000", "15000", "20000"};
+        Integer[] favourites = {0, 5, 10, 15, 20, 25, 30, 40, 50, 100};
 
         // Search text fields
         keyword = new JTextField(20);
-        noOfpage = new JTextField(20);
-        country = new JTextField(20);
-        minPrice = new JTextField(10);
-        maxPrice = new JTextField(10);
-        noOfFavourites = new JTextField(20);
+        noOfpage = new JComboBox<>(pages);
+        country = new JComboBox<>(countryOptions);
+        minPrice = new JComboBox<String>(prices);
+        maxPrice = new JComboBox<String>(prices);
+        noOfFavourites = new JComboBox<Integer>(favourites);
 
         // Search button
         // When pressed, the program will use the Vinted API
         searchButton = new JButton("Search items");
+        // An action listener should be defined here
 
         // Add stuff to the panels and main frame
+        // Filters panel
+        filtersPanel.add(message);
+        filtersPanel.add(new JLabel());
         filtersPanel.add(keywordLabel);
         filtersPanel.add(keyword);
         filtersPanel.add(noOfpageLabel);
@@ -93,14 +116,14 @@ public class MainFrame extends JFrame {
         filtersPanel.add(maxPrice);
         filtersPanel.add(noOfFavouritesLabel);
         filtersPanel.add(noOfFavourites);
-
+        // Buttons panel
         buttonsPanel.add(searchButton);
-
+        // Main frame
         this.add(logoPanel, BorderLayout.NORTH);
         this.add(filtersPanel, BorderLayout.CENTER);
         this.add(buttonsPanel, BorderLayout.SOUTH);
 
+        // We assure the main frame is visible
         this.setVisible(true);
-        filtersPanel.setVisible(true);
     }
 }
