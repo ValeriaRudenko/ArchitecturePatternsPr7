29da9e05ac3ae1863737e8df7d731dd6791c8bc5
Francisco Javier Garcia Gonzalez <alu0101030562@ull.edu.es>
2023-11-29 18:02:51 +0000

    Modificacion de main

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,10 @@
-import es.ull.patrones.api.VintedApiSubject;
-import es.ull.patrones.api.ParseJSON;
-import es.ull.patrones.observer.Observer;
-import es.ull.patrones.observer.PrintObserver;
+import es.ull.patrones.app.App;
 
-import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("Ingrese la página: ");
-    int pagina = scanner.nextInt();
-    System.out.print("Ingrese el precio mínimo: ");
-    int precioMin = scanner.nextInt();
-
-    System.out.print("Ingrese el precio máximo: ");
-    int precioMax = scanner.nextInt();
-    scanner.nextLine();
-    System.out.print("Ingrese el objeto a buscar: ");
-    String objeto = scanner.nextLine();
-
-    // Crear instancia del sujeto
-    VintedApiSubject vintedApiSubject = new VintedApiSubject();
-
-    // Crear instancia del observador
-    Observer printObserver = new PrintObserver();
-
-    // Agregar el observador al sujeto
-    vintedApiSubject.addObserver(printObserver);
-    System.out.println("pagina: " + pagina + "\nprecio min: " + precioMin + "\nprecio max: " + precioMax + "\nArticulo: " + objeto);
-    // Realizar la solicitud HTTP y notificar a los observadores
-    vintedApiSubject.fetchData(pagina, objeto, precioMin, precioMax);
+    App app = new App();
+    app.run();
   }
 }
 
